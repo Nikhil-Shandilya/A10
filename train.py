4fb8f92a875a5dc053042135eebdd282be7516b0
Nikhil-Shandilya <149268205+Nikhil-Shandilya@users.noreply.github.com>
2026-01-15 18:59:17 +0000

    Update train.py

--- train.py
+++ train.py
@@ -5,3 +5,24 @@ X=[[1],[2],[3],[4]]
 y=[4,5,6,7]
 model.fit(X,y)
 joblib.dump(model,"model.pkl")
+
+"""
+pip install fastapi uvicorn scikit-learn pytest joblib
+gedit train.py
+python3 train.py
+gedit main.py
+uvicorn main:app --reload
+curl -X POST http://127.0.0.1:8000/predict -H "Content-Type: application/json" -d '{"x":abc}'
+gedit test_api.py
+pip install "fastapi[all]"
+
+
+pytest
+
+gedit Dockerfile
+
+pip freeze>requirements.txt
+
+Docker build -t test-api-image
+Docker run -p 8000:8000 test-api-image
+"""
